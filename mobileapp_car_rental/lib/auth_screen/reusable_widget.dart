a0ea41efce1ce8844fbfea59e8e929e3f1756272
Karol0114 <karolwalczak61@gmail.com>
2024-03-21 14:41:18 +0100

    Dodane logo do panelu logowania w aplikacji mobilnej

--- mobileapp_car_rental/lib/auth_screen/reusable_widget.dart
+++ mobileapp_car_rental/lib/auth_screen/reusable_widget.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+Image logoWidget(String imageName) {
+  return Image.asset(
+    imageName,
+    fit: BoxFit.fitWidth,
+    width: 240,
+    height: 240,
+    
+  );
+}
+
 TextField reusableTextField(String text, IconData icon, bool isPasswordType,
     TextEditingController controller) {
   return TextField(
