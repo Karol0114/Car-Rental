947300f2f64abc7003faca63e8da910f930e40d5
Karol0114 <karolwalczak61@gmail.com>
2024-05-15 12:45:00 +0200

    Test polaczenia bazy z apka

---
+++ api/api.php
@@ -0,0 +1,27 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$host = 'localhost'; // Adres serwera MySQL
+$dbname = 'wypozyczalnia'; // Nazwa bazy danych
+$username = 'root'; // Nazwa użytkownika
+$password = ''; // Hasło
+
+// Tworzenie połączenia z bazą danych
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Pobranie typu samochodu z parametrów GET
+$type = isset($_GET['type']) ? $_GET['type'] : ' ';
+
+// Zapytanie SQL do pobrania danych o samochodach
+$query = "SELECT marka, model, zdjecie, rok_produkcji, KM, rodzaj_paliwa, pojemnosc_silnika, ilosc_siedzen, ilosc_drzwi, skrzynia_biegow, cena_za_dobe  FROM pojazdy WHERE type = :type";
+$stmt = $pdo->prepare($query);
+$stmt->bindParam(':type', $type, PDO::PARAM_STR);
+$stmt->execute();
+
+$cars = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Zwracanie danych w formacie JSON
+echo json_encode($cars);
+?>
--- mobileapp_car_rental/lib/cars_list_page.dart
+++ mobileapp_car_rental/lib/cars_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp_car_rental/car_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class CarsListScreen extends StatelessWidget {
@@ -11,154 +13,19 @@ class CarsListScreen extends StatelessWidget {
   }) : super(key: key);
   
   // Na razie zwraca statyczną listę samochodów. W przyszłości możesz pobrać odpowiednie dane z bazy.
-  List<Widget> _buildCarsList(String category) {
+  Future<List<dynamic>> fetchCars() async {
     // Załóżmy, że masz funkcję, która generuje listę samochodów na podstawie kategorii
-    
-      // Tutaj wstaw swoje widgety CarTile dla danego rodzaju samochodu
-      if (category == 'SUV') {
-        return [
-          const CarWidget(
-            name: 'Audi', 
-            model: 'Q8', 
-            imageAsset: "assets/images/audi_q8.png",
-            year: 2019,
-            kmh: 507,
-            fuel: 'Diesel', 
-            engine: 4, 
-            seats: 5, 
-            doors: 5, 
-            transmission: 'Automatyczna', 
-            price: 500),
+    final response = await http.get(Uri.parse('http://10.0.2.2:8880/api.php?type=$category'));
 
-            const CarWidget(
-            name: 'Mercedes-Benz', 
-            model: 'GLC 300 e 4-Matic', 
-            imageAsset: "assets/images/mercedes.png", 
-            year: 2021,
-            kmh: 211,
-            fuel: 'Hybryda',  
-            engine: 2, 
-            seats: 5, 
-            doors: 5, 
-            transmission: 'Automatyczna', 
-            price: 250),
-        ];
-      } else if (category == 'Sportowe') {
-        return [
-          const CarWidget(
-            name: 'Porsche', 
-            model: 'Panamera', 
-            imageAsset: "assets/images/Porsche_panamera.png", 
-            year: 2012,
-            kmh: 300,
-            fuel: 'Benzyna',  
-            engine: 3.6, 
-            seats: 4, 
-            doors: 5, 
-            transmission: 'Automatyczna', 
-            price: 300),
 
-          const CarWidget(
-            name: 'Ferrari', 
-            model: '458 Italia', 
-            imageAsset: "assets/images/ferrari.png", 
-            year: 2015,
-            kmh: 569,
-            fuel: 'Benzyna',  
-            engine: 4.5, 
-            seats: 2, 
-            doors: 2, 
-            transmission: 'Automatyczna', 
-            price: 5000),
-        ];
-      } else if (category == 'Luksusowe') {
-        return [
-          const CarWidget(
-            name: 'BMW', 
-            model: 'M4', 
-            imageAsset: "assets/images/bmw_m4.png", 
-            year: 2017,
-            kmh: 431,
-            fuel: 'Benzyna',  
-            engine: 3, 
-            seats: 4, 
-            doors: 2, 
-            transmission: 'Automatyczna', 
-            price: 300),
-
-          const CarWidget(
-            name: 'Audi', 
-            model: 'A7', 
-            imageAsset: "assets/images/audi_a7.png", 
-            year: 2018,
-            kmh: 286,
-            fuel: 'Diesel',  
-            engine: 3, 
-            seats: 5, 
-            doors: 5, 
-            transmission: 'Automatyczna', 
-            price: 280),
-        ];
-      } else if (category == 'Kombi') {
-        return [
-          const CarWidget(
-            name: 'Mercedes-Benz', 
-            model: 'CLA', 
-            imageAsset: "assets/images/mercedes_cla.png", 
-            year: 2019,
-            kmh: 190,
-            fuel: 'Diesel',  
-            engine: 2, 
-            seats: 5, 
-            doors: 5, 
-            transmission: 'Automatyczna', 
-            price: 190),
-
-          const CarWidget(
-            name: 'Volkswagen', 
-            model: 'Passat', 
-            imageAsset: "assets/images/passat.png", 
-            year: 2015,
-            kmh: 180,
-            fuel: 'Benzyna',  
-            engine: 1.8, 
-            seats: 5, 
-            doors: 5, 
-            transmission: 'Automatyczna', 
-            price: 180),
-        ];
-      } else if (category == 'Nasze perełki') {
-        return [
-          const CarWidget(
-            name: 'Fiat', 
-            model: '126', 
-            imageAsset: "assets/images/maluch.jpg", 
-            year: 2000,
-            kmh: 211,
-            fuel: 'Benzyna',  
-            engine: 0.6, 
-            seats: 4, 
-            doors: 2, 
-            transmission: 'Manualna', 
-            price: 10000),
-
-          const CarWidget(
-            name: 'Fiat', 
-            model: 'Multipla', 
-            imageAsset: "assets/images/multipla.jpg", 
-            year: 1999,
-            kmh: 103,
-            fuel: 'Benzyna',  
-            engine: 1.6, 
-            seats: 5, 
-            doors: 5, 
-            transmission: 'Manualna', 
-            price: 15000),
-        ];
-      } else {
-        return [];
-      } 
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load cars');
+    }
   }
+      
+  
 
     @override
   Widget build(BuildContext context) {
@@ -166,8 +33,35 @@ class CarsListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Samochody: $category'),
       ),
-      body: ListView(
-        children: _buildCarsList(category),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchCars(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var car = snapshot.data![index];
+                return CarWidget(
+                  name: car['marka'],
+                  model: car['model'],
+                  imageAsset: car['zdjecie'],
+                  year: car['rok_produkcji'],
+                  kmh: car['KM'],
+                  fuel: car['rodzaj_paliwa'],
+                  engine: double.parse(car['pojemnosc_silnika']),
+                  seats: int.parse(car['ilosc_siedzen']),
+                  doors: int.parse(car['ilosc_drzwi']),
+                  transmission: car['skrzynia_biegow'],
+                  price: double.parse(car['cena_za_dobe']),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
